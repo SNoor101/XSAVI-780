4ccbb19a8194de0dff19f8efb6154f4e5d3a937f
SNoor101 <snoor@pratt.edu>
2017-09-12 18:42:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var number = 6 * 22;
+console.log(number);
+
+confirm("Is this the best website ever?");
+
+prompt("May I ask why you are here?");
+
+var anotherNumber = 33;
+console.log(anotherNumber);
+
+confirm("What are my favorite hobbies or things I do with my 'free' time?")
+
+var thingsIdo = ["eat", "sleep", "walks", "spontaneous trips", "cook", "stress eat"]
+console.log (thingsIdo[0])
+console.log (thingsIdo[1])
+console.log (thingsIdo[2])
+console.log (thingsIdo[3])
+console.log (thingsIdo[4])
+console.log (thingsIdo[5])
+
+thingsIdo.forEach(
+	function printValue(value) {
+		console.log("Array item", value);
+	}
+);
+
+
+var thingsIshoulddo = ["apply to grad school", "get my life together", "more sleep"]
